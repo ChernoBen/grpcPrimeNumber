d2033acde5eb13a2d5b295bd3bded3168ab774e2
ChernoBen <bneto@computacao.cua.ufmt.br>
2021-10-13 14:40:39 -0400

    grpc server streaming

--- src/prime_client/client.go
+++ src/prime_client/client.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"io"
 	"log"
 	"prime/src/prime_proto"
 
@@ -20,5 +22,26 @@ func main() {
 	if c != nil {
 		fmt.Println("Client created")
 	}
-	//doServerStreaming(c)
+	doServerStreaming(c)
+}
+
+func doServerStreaming(c prime_proto.DecompositionClient) {
+	fmt.Println("Inicialização da função")
+	req := &prime_proto.PrimeNumberDecompositionRequest{
+		Number: 1200000,
+	}
+	stream, err := c.PrimeNumberDecomposition(context.Background(), req)
+	if err != nil {
+		log.Fatalf("Erro durante verificação de primo: %v \n", err)
+	}
+	for {
+		res, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("Error ao processar primo %v\n", err)
+		}
+		log.Printf("Número é divisivel por: %v\n", res.GetPrimeFactor())
+	}
 }
